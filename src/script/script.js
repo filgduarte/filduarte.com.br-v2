51cfc7cbfa012e515658025c07e99b19a3ae73e3
Filipe Duarte <filipegduarte@gmail.com>
2024-01-06 12:32:37 -0300

    change: replaced animateSectionTitleOnEnter function with AnimatedTitle component

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,3 @@
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import SplitType from "split-type";
-gsap.registerPlugin(ScrollTrigger);
-
 /**
  * Wraps the children of an element with a wrapper element
  * @param {HTMLElement[]|HTMLElement} parent - The parent element to have children wrapped.
@@ -31,47 +26,4 @@ export function wrapChildren(element, wrapperClass) {
 
     wrapper.appendChild(fragment);
     element.appendChild(wrapper);
-}
-
-/**
- * Animates a text element when it's visible in the viewport
- * @param {string} section - The selector of the section containing the title to be animated.
- * @param {string} title - The selector of the title to be animated.
- * @param {string} start - The position of the section relative to viewport when start the animation.
- * @throws {Error} - If section, title or start are invalid.
- */
-export function animateSectionTitleOnEnter(section, title, start) {
-    if ( typeof section !== 'string' ) {
-        throw new Error('Invalid section selector.');
-    }
-
-    if ( typeof title !== 'string' ) {
-        throw new Error('Invalid title selector.');
-    }
-
-    if (start) {
-        if ( typeof start !== 'string' ) {
-            throw new Error('Invalid start position.');
-        }
-    }
-
-    const splitedTitle = new SplitType(`${section} ${title}`, { types: 'lines' });
-    const timeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: section,
-            start: start ?? '',
-            toggleActions: 'play none play reset',
-        }
-    });
-
-    for (const line of splitedTitle.lines) {
-        wrapChildren(line, 'line-content');
-    }
-
-    timeline
-        .from(`${section} ${title} .line-content`, {
-            y: '100%',
-            duration: 0.5,
-            stagger: 0.15,
-        });
 }
