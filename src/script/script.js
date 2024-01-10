1ad4c578fa8c47fd303a996f0d6fcd04ebb1e746
Filipe Duarte <filipegduarte@gmail.com>
2024-01-10 14:25:30 -0300

    add: menu items click behavior; change: lenis now initialized and exported in scripts/scripts.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,9 @@
+import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 /**
  * Wraps the children of an element with a wrapper element
  * @param {HTMLElement[]|HTMLElement} parent - The parent element to have children wrapped.
@@ -27,3 +33,25 @@ export function wrapChildren(element, wrapperClass) {
     wrapper.appendChild(fragment);
     element.appendChild(wrapper);
 }
+
+/**
+ * Lenis smooth scroll
+ */
+export const lenis = new Lenis();
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add( (time) => {
+    lenis.raf(time * 1000)
+});
+
+gsap.ticker.lagSmoothing(0);
+
+// // Snap sections to top when they enter in the viewport
+// document.querySelectorAll('section').forEach(section => {
+// 	ScrollTrigger.create({
+// 		trigger: section,
+// 		start: 'top 75%',
+// 		onEnter: () => lenis.scrollTo(section, {lock: true}),
+// 	});
+// });
